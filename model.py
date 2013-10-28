13b0360aeba6b7dcd2912dca1e792643c543565a
Hackbright Student <user@mini-005.(none)>
2013-10-28 15:00:14 -0700

    Initial commit

--- model.py
+++ model.py
@@ -1,13 +1,35 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DATETIME
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
 
-### End class declarations
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(32))
+    released_at = Column(DATETIME())
+    imdb_url = Column(String(140))
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer)
+    user_id = Column(Integer)
+    rating = Column(Integer)
+
 
 def main():
     """In case we need this for something"""
