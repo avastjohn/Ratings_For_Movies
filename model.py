b23991e0ea20e35151469bb61d53b6be9474ba93
Hackbright Student <user@mini-005.(none)>
2013-10-29 13:02:38 -0700

    Initial Flask implementation

--- model.py
+++ model.py
@@ -1,31 +1,18 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship, backref
+from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 
-
-ENGINE = None
-Session = None
+# allows querying to session directly
+engine = create_engine("sqlite:///ratings.db", echo=False)
+session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
 
 Base = declarative_base()
+Base.query = session.query_property()
 
 ### Class declarations go here
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key = True)
-    email = Column(String(64), nullable=True)
-    password = Column(String(64), nullable=True)
-    age = Column(Integer, nullable=True)
-    zipcode = Column(String(15), nullable=True)
-
-class Movie(Base):
-    __tablename__ = "movies"
-    id = Column(Integer, primary_key = True)
-    name = Column(String(32), nullable=True)
-    released_at = Column(Date(), nullable=True)
-    imdb_url = Column(String(140), nullable=True)
-
 class Rating(Base):
+    # Association
     __tablename__ = "ratings"
     id = Column(Integer, primary_key = True)
     user_id = Column(Integer, ForeignKey('users.id'))
@@ -33,6 +20,23 @@ class Rating(Base):
     rating = Column(Integer)
     user = relationship("User", backref=backref("ratings", order_by=id))
     movie = relationship("Movie", backref=backref("ratings", order_by=id))
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+    # movie = relationship("Rating", backref="users")
+
+class Movie(Base):
+    __tablename__ = "movies"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(32), nullable=True)
+    released_at = Column(Date(), nullable=True)
+    imdb_url = Column(String(140), nullable=True)
+    # user = relationship("Rating", backref="movies")
 
 # A user has many ratings
 # A rating belongs to a user
